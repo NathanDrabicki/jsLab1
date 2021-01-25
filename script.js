05fbaf814814bfcafb26bb629de712d4a42a1b67
nathan <natedrab@gmail.com>
2021-01-25 16:45:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const myName = "Nathan Drabicki";
+const myAge = 18;
+const myBirthday = "February 22";
+const detroitGC = true;
+const lifeEvents = ["I have lived in Livonia my whole life."
