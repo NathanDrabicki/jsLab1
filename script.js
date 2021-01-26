307b53516b65e0a13048228cd545bc8b063f9855
nathan <natedrab@gmail.com>
2021-01-25 23:10:18 -0500

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-"use strict";
+// "use strict";
 const myName = "Nathan Drabicki";
 const myAge = 18;
 const myBirthday = "February 22";
 const detroitGC = true;
-const lifeEvents = ["I have lived in Livonia my whole life."
+const lifeEvents = [
+  "I have lived in Livonia my whole life.",
+  "I like to play video games, snowboard, and go fishing.",
+  "My favorite food is tacos.",
+  "I have 2 older sisters.",
+];
+
+if (detroitGC) {
+  console.log(
+    `My name is ${myName} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${myAge} years old and my birthday is on ${myBirthday} .`
+  );
+} else {
+  console.log(
+    `My name is ${myName} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${myAge} years old and my birthday is on ${myBirthday} .`
+  );
+}
+for (let i = 0; ; i++) {
+  if (i === 1) {
+    break;
+  }
+  console.log(lifeEvents);
+}
+const counter = 0;
+while (i === 0) {
+  console.log(counter);
+}
+const randomNumber = 6;
+{
+  Math.floor(Math.random() * 10);
+}
+if (randomNumber) {
+}
+// Math.random
+// Math.floor
+
+// while loop----BREAK STATEMENT
